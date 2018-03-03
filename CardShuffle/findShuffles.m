624014947b0aceb781fc59e111b66d0f7069703c
Adam Simon Chatterley <aschatterley@gmail.com>
2018-03-03 15:41:17 +0100

    now return all possible orders that lead to n shuffles

--- CardShuffle/findShuffles.m
+++ CardShuffle/findShuffles.m
@@ -1,26 +1,19 @@
 %start with ordered list: 1 2 3 4
 %for a given list:
-%   find all n>1 which are in their place (list == [1 2 3 4]
-%   	each 1, repeat recursively
-%       all 0: return number of hops and current list
+%   find all n>1 which are in their place (list == [1 2 3 4])
+%   	for each 1, repeat recursively
+%       if all are 0: return number of hops and current list
+%dies around n=13
 
 %for 13 (80 shuffles):
 %[2 9 4 5 11 12 10 1 8 13 3 6 7]
 
-% best = 1;
-% for p=perms(2:10)'
-%     [totalShuffles, cardList] = findJumps([1 p'], 0);
-%     if totalShuffles > best
-%         fprintf('best n = %d\n', totalShuffles);
-%         disp(cardList);
-%         best = totalShuffles;
-%     end
-% end
-    
-% 
-function [totalShuffles, cardList] = findShuffles(nCards)
-cards = 1:nCards;
+%for 53, I estimate 1e7, based on an exponential fit to the first 13
+%solutions
+
+function [cardList, totalShuffles] = findShuffles(cards)
  [totalShuffles, cardList] = findJumps(cards, 0);
+ totalShuffles = totalShuffles(1);
 end
 
 function [nHops, list] = findJumps(cards, n)
@@ -30,15 +23,18 @@ function [nHops, list] = findJumps(cards, n)
         list = cards;
         return
     else
-        hops = zeros(sum(inOrder), 1);
-        lists = zeros(sum(inOrder), length(cards));
+        hops = zeros(sum(inOrder)*50, 1);
+        lists = zeros(sum(inOrder)*50, length(cards));
         m = 1;
         for i=find(inOrder)
             prevCards = [flip(cards(1:i)), cards(i+1:end)];
-            [hops(m), lists(m,:)] = findJumps(prevCards, n+1);
-            m = m+1;
+            [h, l] = findJumps(prevCards, n+1);
+            hops(m:m+length(h)-1) = h;
+            lists(m:m+length(h)-1, :) = l;
+            m = m+length(h);
         end
-        [nHops, ind] = max(hops);
+        ind = (hops == max(hops));
+        nHops = hops(ind);
         list = lists(ind, :);
         return
     end
